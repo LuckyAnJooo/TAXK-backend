ef09820ca05060d22c6b41451012ae6ecdd30357
CapAndy <13100192+z1114_a@user.noreply.gitee.com>
2026-03-27 17:27:24 +0800

    First commit for push the initial project

---
+++ src/test/java/com/example/TAXK/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TAXK.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
