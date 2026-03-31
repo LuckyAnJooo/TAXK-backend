9995ac332d5e0a905d9bd24345c8cd1577dd8a85
cNormal777 <tal025@ucsd.edu>
2026-03-31 05:34:22 +0000

    Implemented StockPriceService

--- src/main/java/com/example/TAXK/demo/service/StockPriceService.java
+++ src/main/java/com/example/TAXK/demo/service/StockPriceService.java
@@ -6,16 +6,31 @@ import yahoofinance.histquotes.HistoricalQuote;
 import yahoofinance.histquotes.Interval;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.Map;
 
 @Service
 public class StockPriceService {
+    // get the price of a stock
+    public Optional<Double> getStockPrice(String ticker) {
+        Optional<Map<String, Object>> result = validate(ticker);
+        if(result.isPresent()) {
+            Map<String, Object> data = result.get();
+            return Optional.of((double) data.get("currentPrice"));
+        }
+        return Optional.empty();
+    }
     // Get the prices of multiple stocks
     public Map<String, Stock> getAllStocks(List<String> tickers) {
-        //TODO
-        return new HashMap<String, Stock>();
+        try {
+            String arr[] = tickers.toArray(new String[0]);
+            Map<String, Stock> results = YahooFinance.get(arr);
+            return results;
+        } catch (IOException e){
+            return Collections.emptyMap();
+        }
     }
 
     // Get historical price of one single stock
@@ -24,6 +39,23 @@ public class StockPriceService {
         return new LinkedList<HistoricalQuote>();
     }
 
-    // Validate if this ticket exists and return the information
-
+    // Validate if this ticket exists and return the stock information
+    public Optional<Map<String, Object>> validate(String ticker) {
+         try {
+             Stock stock = YahooFinance.get(ticker.toUpperCase());
+             if(stock == null || stock.getQuote().getPrice() == null) {
+                 //doesn't exist
+                 return Optional.empty();
+             }
+             Map<String, Object> result = new HashMap<String, Object>();
+             result.put("ticker", stock.getSymbol());
+             result.put("name", stock.getName());
+             result.put("exchange", stock.getStockExchange());
+             result.put("currentPrice", stock.getQuote().getPrice());
+             result.put("currency", stock.getCurrency());
+             return Optional.of(result);
+         } catch(IOException e) {
+             return Optional.empty();
+        }
+    }
 }
