4fcbc919c7d20d3c32754b98f66506ea27bbf64e
xavier <2206250012@qq.com>
2026-03-31 13:34:25 +0800

    Buy Service done

--- src/main/java/com/example/TAXK/demo/repo/HoldingRepo.java
+++ src/main/java/com/example/TAXK/demo/repo/HoldingRepo.java
@@ -1,10 +1,12 @@
 package com.example.TAXK.demo.repo;
 
 import com.example.TAXK.demo.entity.Holding;
-import com.fasterxml.jackson.annotation.JacksonAnnotation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HoldingRepo extends JpaRepository<Holding, Long> {
+    Optional<Holding> findByTicker(String ticker);
 }
