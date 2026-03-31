fe0f15ce24b3ad250cc75a704bdb4748e46b85a6
xavier <2206250012@qq.com>
2026-03-31 13:34:25 +0800

    Buy Service done

---
+++ src/main/java/com/example/TAXK/demo/service/PortfolioService.java
@@ -0,0 +1,80 @@
+package com.example.TAXK.demo.service;
+
+import com.example.TAXK.demo.entity.Holding;
+import com.example.TAXK.demo.entity.Transaction;
+import com.example.TAXK.demo.repo.HoldingRepo;
+import com.example.TAXK.demo.repo.TransactionRepo;
+import org.springframework.stereotype.Service;
+import yahoofinance.Stock;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class PortfolioService {
+    public final TransactionRepo transactionRepo;
+    public final HoldingRepo holdingRepo;
+    public final StockPriceService stockPriceService;
+
+    public PortfolioService(TransactionRepo transactionRepo, HoldingRepo holdingRepo, StockPriceService stockPriceService) {
+        this.transactionRepo = transactionRepo;
+        this.holdingRepo = holdingRepo;
+        this.stockPriceService = stockPriceService;
+    }
+
+
+    // buy
+
+    public void buy(String ticker, int quantity) {
+        // Step 1: 查价
+        Map<String, Stock> stocks = stockPriceService.getAllStocks(List.of(ticker));
+        double currentPrice = stocks.get(ticker).getQuote().getPrice().doubleValue();
+
+        // Step 2: 查库
+        Optional<Holding> existing = holdingRepo.findByTicker(ticker);
+
+        // 跟新两个表
+
+        // a.存在 -> 更新，b.不存在 -> 新纪录
+        if (existing.isPresent()) {
+            // Step 3a: 存在
+            Holding holding = existing.get();
+            double oldQty = holding.getQuantity();
+            double oldAvgCost = holding.getAverage_cost();
+
+            double newQty = oldQty + quantity;
+            double newAvgCost = (oldQty * oldAvgCost + quantity * currentPrice) / newQty;
+
+            holding.setQuantity((int) newQty);
+            holding.setAverage_cost(newAvgCost);
+            holding.setUpdated_at(LocalDateTime.now());
+            holdingRepo.save(holding);
+        } else {
+            // Step 3b: 不存在
+            Holding newHolding = new Holding();
+            newHolding.setTicker(ticker);
+            newHolding.setQuantity(quantity);
+            newHolding.setAverage_cost(currentPrice);
+            newHolding.setFirst_buy_date(LocalDate.now());
+            newHolding.setCreated_at(LocalDateTime.now());
+            newHolding.setUpdated_at(LocalDateTime.now());
+            holdingRepo.save(newHolding);
+        }
+
+        // Step 4: 跟新交易表
+        Transaction tx = new Transaction();
+        tx.setTicker(ticker);
+        tx.setType(Transaction.TransactionType.BUY);
+        tx.setQuantity(quantity);
+        tx.setPrice(currentPrice);
+        tx.setTotalAmount(quantity * currentPrice);
+        tx.setRealizedPnl(0);
+        tx.setTradeDate(LocalDate.now());
+        tx.setCreatedAt(LocalDateTime.now());
+        transactionRepo.save(tx);
+    }
+
+}
