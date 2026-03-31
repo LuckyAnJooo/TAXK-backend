09bbe31dfe72f86fe270efbc65100bd7303098b2
CapAndy <13100192+z1114_a@user.noreply.gitee.com>
2026-03-31 13:42:52 +0800

    add a template for controller
    # Conflicts:
    #	src/main/java/com/example/TAXK/demo/controller/HoldingController.java

--- src/main/java/com/example/TAXK/demo/controller/HoldingController.java
+++ src/main/java/com/example/TAXK/demo/controller/HoldingController.java
@@ -5,10 +5,7 @@ import com.example.TAXK.demo.repo.HoldingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,15 +14,23 @@ import java.util.List;
 @CrossOrigin
 public class HoldingController {
 
-    @Autowired
-    private HoldingRepo holds;
+    private final PortfolioService portfolioService;
 
-    @GetMapping("/holding")
-    public ResponseEntity<List<Holding>> getHolding(){
+//    @GetMapping("api/holding")
+//    public ResponseEntity<List<Holding>> getHolding(){
+//        List<Holding> holds = holdsRepo.findAll();
+//        System.out.println(holds);
+//        return ResponseEntity.status(HttpStatus.OK).body(holds);  // 正确返回数据
+//    }
 
-
-        return null;
+    @PostMapping("api/portfolio")
+    public ResponseEntity<> addHolding(@RequestBody Holding holding){
+        Holding savedHolding = portfolioService.buy(holding);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedHolding);
     }
+
+
+
 
 
 
